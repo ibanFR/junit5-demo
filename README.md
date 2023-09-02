ea1acf967298dbe53d11e1fa90d6e67d88103672
ibanFR <ibanfernandezrojo@msn.com>
2023-09-02 17:40:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# JUnit 5 demo project
+JUnit 5 playground project to showcase following themes:
+- Test cases as executable specifications using @DisplayName and @DisplayNameGeneration.
+- Maven surefire junit5-tree reporter.
+
+## Display Name Generation
+Use @DisplayName and @DisplayNameGeneration to improve test readability.
+
+## Junit5 tree reporter
+Use https://github.com/fabriciorby/maven-surefire-junit5-tree-reporter to add a tree view for the unit tests executed 
+using JUnit5.
+
+run `mvn test`
---
+++ src/test/java/com/ibanfr/displayname/DisplayNameGeneratorDemoTest.java
@@ -0,0 +1,62 @@
+package com.ibanfr.displayname;
+
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Display Name Generation")
+class DisplayNameGeneratorDemoTest {
+
+    @Test
+    @DisplayName("should describe the behavior under test")
+    void should_pass() {
+        Assertions.assertTrue(true);
+    }
+
+    @Nested
+    @DisplayName("When @DisplayName annotation is used")
+    class WhenDisplayNameAnnotationIsUsed {
+
+        @Test
+        @DisplayName("should display the content of the @DisplayName annotation")
+        void should_use_displayName_annotated_content(TestInfo testInfo) {
+            System.out.println(testInfo.getDisplayName());
+            assertThat(testInfo.getDisplayName()).contains("should display the content of the @DisplayName annotation");
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class When_DisplayNameGeneration_is_ReplaceUnderscores {
+
+        @Test
+        void should_replace_underscores_with_spaces(TestInfo testInfo) {
+            System.out.println(testInfo.getDisplayName());
+            assertThat(testInfo.getDisplayName()).contains("should replace underscores with spaces");
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(ReplaceCamelCase.class)
+    class WhenDisplayNameGenerationIsReplaceCamelCase {
+
+        @Test
+        void shouldReplaceCamelCase(TestInfo testInfo) {
+            System.out.println(testInfo.getDisplayName());
+            assertThat(testInfo.getDisplayName()).contains("should replace camel case");
+        }
+
+    }
+
+    @Nested
+    @IndicativeSentencesGeneration(separator = " -> ", generator = ReplaceCamelCase.class)
+    class WhenIndicativeSentencesGenerationIsEnabled {
+        @Test
+        void shouldDisplayClassNameAndTestName(TestInfo testInfo) {
+            System.out.println(testInfo.getDisplayName());
+
+            assertThat(testInfo.getDisplayName()).contains("When indicative sentences generation is enabled -> should display class name and test name");
+        }
+    }
+    
+}
